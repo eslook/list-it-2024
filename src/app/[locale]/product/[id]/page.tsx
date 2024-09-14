0190d2af5a747e0e7f59a5f76a382c4011f61e6d
eslook <charr93@hotmail.com>
2024-09-14 10:58:33 +0200

    chore: format page.tsx

--- src/app/[locale]/product/[id]/page.tsx
+++ src/app/[locale]/product/[id]/page.tsx
@@ -4,7 +4,7 @@ import { unstable_setRequestLocale } from 'next-intl/server';
 export default async function Page({
   params: { locale, id },
 }: {
-  params: { locale: string, id: string };
+  params: { locale: string; id: string };
 }) {
   unstable_setRequestLocale(locale);
   return <p>Detail {id}</p>;
