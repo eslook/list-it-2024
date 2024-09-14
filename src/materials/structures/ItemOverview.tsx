de2cd024e1d356e31130c37dbdab08de5b16ca8b
eslook <charr93@hotmail.com>
2024-09-14 19:27:03 +0200

    feat: display information on item overview

--- src/materials/structures/ItemOverview.tsx
+++ src/materials/structures/ItemOverview.tsx
@@ -2,6 +2,7 @@
 
 import { use } from 'react';
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 
 interface ItemOverviewProps {
   itemPromise: Promise<ApiProduct>;
@@ -14,6 +15,18 @@ const ItemOverview: React.FC<ItemOverviewProps> = ({ itemPromise }) => {
     <>
       <h1>{t('title')}</h1>
       <p>{item.name}</p>
+      <p>{item.brand}</p>
+      <p>{item.category}</p>
+      <Image src={item.image} alt="" width={600} height={600} />
+      <p>{t('specifications.title')}</p>
+      <dl>
+        <dt>{t('specifications.processor')}</dt>
+        <dd>{item.specifications.processor}</dd>
+        <dt>{t('specifications.ram')}</dt>
+        <dd>{item.specifications.ram}</dd>
+        <dt>{t('specifications.storage')}</dt>
+        <dd>{item.specifications.storage}</dd>
+      </dl>
     </>
   );
 };
