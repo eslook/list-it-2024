5697b19d5a21d0f273e071a49d81f9a5e12c2ed8
eslook <charr93@hotmail.com>
2024-09-15 14:46:21 +0200

    feat: generate localized meta data for not-found

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import { useTranslations } from 'next-intl';
 import MetaPageBlock from '@/materials/structures/MetaPageBlock';
+import { Metadata, ResolvingMetadata } from 'next';
+import { getTranslations } from 'next-intl/server';
+
+export async function generateMetadata(
+  {
+    params,
+  }: {
+    params: { locale: string };
+  },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const t = await getTranslations({
+    locale: params.locale,
+    namespace: 'page.notFound',
+  });
+  const parentTitle = (await parent).title?.absolute;
+  const titles = [t('title'), parentTitle].filter(Boolean);
+
+  return {
+    title: titles.join(' | '),
+  };
+}
 
 export default function Page() {
   const t = useTranslations('page.notFound');
