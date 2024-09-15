48a9bfa5c2152f3b8442d4d16ca13ee38ddd0f79
eslook <charr93@hotmail.com>
2024-09-15 14:33:10 +0200

    chore: wrap storybook in IntlProvider

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react';
 import React from 'react';
+import { IntlProvider } from 'next-intl';
+import messages from '../messages/en-US.json';
 import { darkerGrotesqueClassName } from '@/utils/googleFontClassNames';
 import '@/styles/globals.scss';
 import './storybook.scss';
@@ -15,7 +17,13 @@ const preview: Preview = {
   tags: ['autodocs'],
   decorators: [
     (Story, context) => {
-      return <div className={darkerGrotesqueClassName}>{Story()}</div>;
+      return (
+        <div className={darkerGrotesqueClassName}>
+          <IntlProvider locale="en-US" messages={messages}>
+            {Story()}
+          </IntlProvider>
+        </div>
+      );
     },
   ],
 };
