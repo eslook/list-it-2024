c44c11eb1c614ff8675f40aa803c3354113a62aa
eslook <charr93@hotmail.com>
2024-09-15 14:48:47 +0200

    feat: update layout view with new styled materials

--- src/materials/views/Layout/Layout.tsx
+++ src/materials/views/Layout/Layout.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './layout.module.scss';
 import { darkerGrotesqueClassName } from '@/utils/googleFontClassNames';
-import { Link } from '@/i18n/routing';
+import Header from '@/materials/structures/Header';
+import Footer from '@/materials/structures/Footer';
 
 interface LayoutProps {
   /**
@@ -13,30 +14,23 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className={[styles.layout, darkerGrotesqueClassName].join(' ')}>
+      <Header />
+      <noscript>
+        <p>
+          This website works best with JavaScript enabled. Here&apos;s a{' '}
+          <a
+            href="https://www.enable-javascript.com/"
+            target="_blank"
+            rel="noopener">
+            guide to help you turn it on
+          </a>
+          .
+        </p>
+      </noscript>
       <main id="main-content" className={styles.main}>
-        <Link href="/" locale="me-OW">
-          Switch to Cat
-        </Link>
-        <Link href="/" locale="en-US">
-          Switch to English
-        </Link>
-        <Link href="/item/1">Switch to Detail 1</Link>
-        <Link href="/lists">Switch to Lists</Link>
-
-        <noscript>
-          <p>
-            This website works best with JavaScript enabled. Here&apos;s a{' '}
-            <a
-              href="https://www.enable-javascript.com/"
-              target="_blank"
-              rel="noopener">
-              guide to help you turn it on
-            </a>
-            .
-          </p>
-        </noscript>
         {children}
       </main>
+      <Footer />
     </div>
   );
 };
