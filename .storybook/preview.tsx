307a5cde755adcde2ab6db37085f4b527d76a553
eslook <charr93@hotmail.com>
2024-09-15 14:32:23 +0200

    chore: storybook style for improved preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/react';
 import React from 'react';
 import { darkerGrotesqueClassName } from '@/utils/googleFontClassNames';
 import '@/styles/globals.scss';
+import './storybook.scss';
 
 const preview: Preview = {
   parameters: {
