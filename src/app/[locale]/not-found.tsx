740429018c2e3fd5f8e80e6428ea2d7b32df946c
eslook <charr93@hotmail.com>
2024-09-15 14:45:58 +0200

    feat: use meta page block on not found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useTranslations } from 'next-intl';
+import MetaPageBlock from '@/materials/structures/MetaPageBlock';
 
 export default function Page() {
   const t = useTranslations('page.notFound');
-  return <p>{t('title')}</p>;
+  return (
+    <MetaPageBlock
+      title={t('title')}
+      description={t('description')}
+      action={t('action')}
+      code="404"
+    />
+  );
 }
